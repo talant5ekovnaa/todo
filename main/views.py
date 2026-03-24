e8490d01bf6b56017feb7a1809e3002019157e84
talant5ekovnaa <fjeenbaeva@gmail.com>
2026-03-24 21:16:05 +0600

    added todo deletion

--- main/views.py
+++ main/views.py
@@ -22,3 +22,9 @@ def add_todo(request):
     todo.save()
     
     return redirect(test)
+
+
+def delete_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.delete()
+    return redirect(test)
