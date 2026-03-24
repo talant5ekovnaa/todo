1fd76e92676effa5fecbf592a6607a122777fbc8
talant5ekovnaa <fjeenbaeva@gmail.com>
2026-03-24 19:28:19 +0600

    set static and media urls and roots

--- main/models.py
+++ main/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class ToDo(models.Model):
+    text = models.CharField(max_length=100)
+    created_at = modele.DataField(auto_now_add=True)
